6193b2b4cd1486be52870026ab88227653e39bc9
Ulrich Sommer <mail@ulrich-sommer.de>
2019-03-12 18:09:55 +0100

    MQTT Processing improved for command and status handling

--- snet-sprinkler.py
+++ snet-sprinkler.py
@@ -1,24 +1,62 @@
-#!./bin/python
+#!./bin/python3
+
+#command:
+# line1.mode = auto, semi, manu
+# line1.value = 1-999
+# line1.active = ON,OFF
+#status:
+# line1.mode
+# line1.value
+# line1.active
+# line1.temperature
+# line1.humidity
+
 
 virtualmode = True
 
+line1_mode = "manu"
+line2_mode = "manu"
+line3_mode = "manu"
+
+line1_manu_irrigationtime = 5
+line2_manu_irrigationtime = 5
+line3_manu_irrigationtime = 5
+
+line1_semi_irrigationtime = 5
+line2_semi_irrigationtime = 5
+line3_semi_irrigationtime = 5
+
+line1_auto_sensitivity = 500
+line2_auto_sensitivity = 500
+line3_auto_sensitivity = 500
+
+
+
+import schedule # for running cron like tasks (schedule.readthedocs.io)
 import time  # for waiting in the code
 import sys  # for exiting the code if an error occurs
 import paho.mqtt.client as mqtt #for processing mqtt messages
-import csv
+import csv # for importing the mqtt credentials
+import configparser #for reading the configuration file
+
+config = configparser.ConfigParser()
+config.read('snet-sprinkler.conf')
+
+mqttpublishstatus = config['MQTT Settings']['domain'] + "/" + config['MQTT Settings']['hostname'] + "/status"
+mqttpublishdebug = config['MQTT Settings']['domain'] + "/" + config['MQTT Settings']['hostname'] + "/debug"
 
 if virtualmode == False:
-	print ("Setting up GPIO pins...")
-	print ("Setting up RS485 connections...")
-elif virtualmode == True:
+    print ("Setting up GPIO pins...")
+    print ("Setting up RS485 connections...")
     import chirp_modbus
-    line-1-sensor = chirp_modbus.SoilMoistureSensor(address=1, serialport='/dev/ttyUSB0')
-try:
-	line-1-sensor.getMoisture()
-    line-1-sensor.getTemperature()
-except (IOError, ValueError):
-    print("Error reading sensor data...")
-
+    line1_sensor = chirp_modbus.SoilMoistureSensor(address=1, serialport='/dev/ttyUSB0')
+    try:
+        line1_sensor.getMoisture()
+        line1_sensor.getTemperature()
+    except (IOError, ValueError):
+        print("Error reading sensor data...")
+elif virtualmode == True:
+    print("Entering simulation mode...")
 mqttclient = mqtt.Client("snet-sprinkler")
 todolist_time = dict()
 
@@ -38,32 +76,17 @@ def mqttconnected(client, userdata, flags, rc):
         print (mqtterrorcode.get(rc, "Error code unknown..."))
 
 def mqtt_connect():
-    try:
-        with open('mqtt_credentials.csv', 'r') as f:
-            reader = csv.reader(f)
-            data = next(reader)  # Skip header line
-            data = next(reader)
-            mqtt_credentials_server = data[0]
-            mqtt_credentials_user = data[1]
-            mqtt_credentials_password = data[2]
-            mqtt_credentials_port = int(data[3])
-            mqtt_credentials_sslport = int(data[4])
-            mqtt_credentials_websocketport = int(data[5])
-        print("successfully imported MQTT credentials...")
 
-    except:
-        print("error opening mqtt_credentials.csv. Aborting...")
-        sys.exit()
-
-    mqttclient.username_pw_set(mqtt_credentials_user, password=mqtt_credentials_password)
+    mqttclient.username_pw_set(config['MQTT Credentials']['user'], password=config['MQTT Credentials']['password'])
     mqttclient.on_message=mqtt_message_recieved
     mqttclient.on_subscribe=mqttsubscribed
     mqttclient.on_connect=mqttconnected
-    mqttclient.connect(mqtt_credentials_server, port=mqtt_credentials_port, keepalive=60, bind_address="")
+    mqttclient.connect(config['MQTT Credentials']['server'], port=int(config['MQTT Credentials']['port']), keepalive=60, bind_address="")
 
     mqttclient.loop_start()
-    mqttclient.subscribe("kirchenfelder75/snet-sprinkler/command/#")
-    mqttclient.publish("kirchenfelder75/snet-sprinkler/debug","hello from snet-sprinkler")
+    mqttsubscribe = config['MQTT Settings']['domain'] + "/" + config['MQTT Settings']['hostname'] + "/command/#"
+    mqttclient.subscribe(mqttsubscribe)
+    mqttclient.publish(mqttpublishdebug,"hello from snet-sprinkler")
 
 def mqttsubscribed(client, userdata, mid, granted_qos):
     print ("successfully subscribed to MQTT topic with qos levels:", granted_qos)
@@ -71,42 +94,176 @@ def mqttsubscribed(client, userdata, mid, granted_qos):
 def mqtt_message_recieved(client, userdata, message):
     mqtttopic=str(message.payload.decode("utf-8"))
     print("message received" ,mqtttopic, "/ message topic =",message.topic, "/ message qos =", message.qos, "/ message retain flag =", message.retain)
-    taskmsg = message.topic.split("command/")[1]
-    if (taskmsg == "line-1"  or taskmsg == "line-2" or taskmsg == "line-3"):
-        channel = taskmsg
-        try:
-            latchingtime = int(mqtttopic)
-        except:
-            latchingtime = 5000
-        switch_channel(channel,"ON")
-        todolist_time[channel] = [int(round(time.time() * 1000)), latchingtime, 0]
+    topicmsg = message.topic.split("command/")[1]
+    channel = topicmsg.split("/")[0]
+    channelsetting= topicmsg.split("/")[1]
+
+    if (channel == "line1"  or channel == "line2" or channel == "line3"):
+        processchannelsettings(channel,channelsetting, mqtttopic)
+
+def processchannelsettings(channel, channelsetting, value):
+
+    global line1_mode
+    global line2_mode
+    global line3_mode
+
+    global line1_manu_irrigationtime
+    global line2_manu_irrigationtime
+    global line3_manu_irrigationtime
+
+    global line1_semi_irrigationtime
+    global line2_semi_irrigationtime
+    global line3_semi_irrigationtime
+
+    global line1_auto_sensitivity
+    global line2_auto_sensitivity
+    global line3_auto_sensitivity
+
+    if (channelsetting == "mode"):
+        if (channel == "line1"):
+            print("changing mode of line1 to", value)
+
+
+            line1_mode = value
+        elif (channel == "line2"):
+            line2_mode = value
+            print("changing mode of line2 to", value)
+        elif (channel == "line3"):
+            line3_mode = value
+            print("changing mode of line3 to", value)
+    elif (channelsetting == "value"):
+        if (channel == "line1"):
+            if (line1_mode == "manu"):
+                print("Setting line1_manu_irrigationtime to", value)
+                line1_manu_irrigationtime = int(value)
+            elif (line1_mode == "semi"):
+                print("Setting line1_semi_irrigationtime to", value)
+                line1_semi_irrigationtime = int(value)
+            elif (line1_mode == "auto"):
+                print("Setting line1_auto_sensitivity to", value)
+                line1_auto_sensitivity = int(value)
+        elif (channel == "line2"):
+            if (line1_mode == "manu"):
+                print("Setting line2_manu_irrigationtime to", value)
+                line2_manu_irrigationtime = int(value)
+            elif (line2_mode == "semi"):
+                print("Setting line2_semi_irrigationtime to", value)
+                line2_semi_irrigationtime = int(value)
+            elif (line2_mode == "auto"):
+                print("Setting line2_auto_sensitivity to", value)
+                line2_auto_sensitivity = int(value)
+        elif (channel == "line3"):
+            if (line1_mode == "manu"):
+                print("Setting line3_manu_irrigationtime to", value)
+                line3_manu_irrigationtime = int(value)
+            elif (line3_mode == "semi"):
+                print("Setting line3_semi_irrigationtime to", value)
+                line3_semi_irrigationtime = int(value)
+            elif (line3_mode == "auto"):
+                print("Setting line3_auto_sensitivity to", value)
+                line3_auto_sensitivity = int(value)
+
+    elif (channelsetting == "active"):
+        if (channel == "line1"):
+            if (line1_mode == "manu"):
+                if (value == "ON"):
+                    switch_channel(channel,"ON")
+                    todolist_time[channel] = [int(round(time.time() * 1000)), line1_manu_irrigationtime*1000, "OFF"]
+                elif (value == "OFF"):
+                    switch_channel(channel,"OFF")
+                    removetodolist_time(channel)
+    update_mqtt_status(channel)
+
+def update_mqtt_status(channel):
+    mqttpublishstring = mqttpublishstatus + "/" + channel + "/" + "mode"
+    if (channel == "line1"):
+        value = line1_mode
+    elif (channel == "line2"):
+        value = line2_mode
+    elif (channel == "line3"):
+        value = line3_mode
+    mqttclient.publish(mqttpublishstring,value)
+
+    mqttpublishstring = mqttpublishstatus + "/" + channel + "/" + "value"
+    if (channel == "line1"):
+        if (line1_mode == "manu"):
+            value = line1_manu_irrigationtime
+        elif (line1_mode == "semi"):
+            value = line1_semi_irrigationtime
+        elif (line1_mode == "auto"):
+            value = line1_auto_sensitivity
+    if (channel == "line2"):
+        if (line2_mode == "manu"):
+            value = line2_manu_irrigationtime
+        elif (line2_mode == "semi"):
+            value = line2_semi_irrigationtime
+        elif (line2_mode == "auto"):
+            value = line2_auto_sensitivity
+    if (channel == "line3"):
+        if (line3_mode == "manu"):
+            value = line3_manu_irrigationtime
+        elif (line3_mode == "semi"):
+            value = line3_semi_irrigationtime
+        elif (line3_mode == "auto"):
+            value = line3_auto_sensitivity
+    mqttclient.publish(mqttpublishstring,value)
+
 
 def switch_channel(channel,state):
-
-    if (channel=="line-1"):
-        if (virtualmode == True):
+    if (virtualmode == True):
+        print("Pretending to set channel",channel,"to",state)
+    elif (virtualmode == False):
+        if (channel=="line1"):
             print("Setting channel", "1", "to", state)
-    if (channel=="line-2"):
-        if (virtualmode == True):
+        if (channel=="line2"):
             print("Setting channel", "2", "to", state)
-    if (channel=="line-3"):
-        if (virtualmode == True):
+        if (channel=="line3"):
             print("Setting channel", "3", "to", state)
 
+    mqttpublishstring = mqttpublishstatus + "/" + channel + "/" + "active"
+    mqttclient.publish(mqttpublishstring,state)
+
+def removetodolist_time(channel):
+    poplist = set()
+    for todolistitem in todolist_time:
+        if (todolistitem == channel):
+            poplist.add(todolistitem)
+    for popitem in poplist:
+        todolist_time.pop(popitem)
 
 def checktodolist_time():
     poplist = set()
     for todolistitem in todolist_time:
         if (int(round(time.time() * 1000)) - (todolist_time[todolistitem][0]) > (todolist_time[todolistitem][1]) ):
-
-            switch_channel(todolistitem,"OFF")
+            switch_channel(todolistitem, todolist_time[todolistitem][2])
             poplist.add(todolistitem)
     for popitem in poplist:
         todolist_time.pop(popitem)
 
 mqtt_connect()
 
+def job():
+    if (line1_mode == "auto"):
+        print ("automatic mode not implemented yet...")
+    elif (line1_mode == "semi-auto"):
+        print ("Starting irrigation of line1 for", line1_irrigation_time, "s")
+        switch_channel("line1","ON")
+        todolist_time[channel] = [int(round(time.time() * 1000)), line1_irrigation_time*1000, "OFF"]
+
+
+
+
+#schedule.every().minute.at(":00").do(irrigationjob)
+#schedule.every().hour.do(job)
+#schedule.every().day.at("10:30").do(job)
+#schedule.every(5).to(10).minutes.do(job)
+#schedule.every().monday.do(job)
+#schedule.every().wednesday.at("13:15").do(job)
+#schedule.every().minute.at(":17").do(job)
+
+
 # the main loop
 while True:
+    schedule.run_pending()
     checktodolist_time()
     time.sleep(1)
